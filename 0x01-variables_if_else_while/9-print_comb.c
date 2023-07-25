60ea2614f9853777aef94c4b954871e96abbdb54
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-25 08:32:47 -0700

    commit by file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers with putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
