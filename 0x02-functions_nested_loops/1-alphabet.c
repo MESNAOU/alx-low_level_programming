249363208558a0b18138d630b833578c08b6710a
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-25 14:51:08 -0700

    commit by file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-/**
- * print_alphabet - print alphabet with putchar
- */
-void print_alphabet(void)
-{
-	char x;
 
-	for (x = 'a'; x <= 'z'; x++)
-	{
-		putchar(x);
-	}
-	putchar('\n');
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
-#include "1-alphabet.c"
+#include <stdio.h>
+/**
+ * print_alphabet - print alphabet with putchar
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
