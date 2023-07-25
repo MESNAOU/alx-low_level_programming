68044fd8e8062071a3c9bc697f800c71fb83dc28
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-24 17:14:54 -0700

    commit by file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	r=n%10;
+	r = n % 10;
 	printf("Last digit of %d is %d ", n, r);
 	if (r > 5)
 	{
 		printf("and is greater than 5\n");
-	}else if (r == 0)
+	} else if (r == 0)
 	{
 		printf("and is 0\n");
-	}else
+	} else
 	{
 		printf("and is less than 6 and not 0\n");
 	}
