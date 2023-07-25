a1b8971f8d434dc353b3f9af4dea7d5ebd292411
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-25 15:39:31 -0700

    commit by file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * print_alphabet - print alphabet with putchar
+ * print_alphabet_x10 - print alphabet with putchar
  */
 void print_alphabet_x10(void)
 {
 	char x;
 	int i;
 
-	for (i = 0;i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 		{
