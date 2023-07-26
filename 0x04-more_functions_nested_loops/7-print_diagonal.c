ac9a033dc5d525e54ba1a0a829f69fcfb9a4122c
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 16:35:06 -0700

    commit by file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - draw a diagonal
+ * @n: length of the diagonal
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\');
+		i++;
+	}
+	_putchar('\n');
+}
