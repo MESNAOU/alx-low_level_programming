889b230ea426a4114836dc5c4d24a0a5a6ae75ba
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 05:24:09 -0700

    commit by file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *jack_bauer - modilaze a clock
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	while (i < 24)
+	{
+		while (j < 60)
+		{
+			_putchar(48 + (i / 10));
+			_putchar(48 + (i % 10));
+			_putchar(':');
+			_putchar(48 + (j / 10));
+			_putchar(48 + (j % 10));
+			_putchar('\n');
+		}
+	}
+}
