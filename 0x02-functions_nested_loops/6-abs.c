12b1198ec783722fcef08a580292ed83f948ea31
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 02:59:59 -0700

    commit by file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+ * _abs - get the absolute value of a number
+ *@n: char to test
+ *
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1*n);
+	} else
+	{
+		return (n);
+	}
+}
