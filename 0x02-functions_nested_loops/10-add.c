ec5b3655936328c7d98660c01db4f1fb7124341c
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 06:02:24 -0700

    commit by file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,12 @@
-#include <include> 
+#include "main.h"
+/**
+ * add - add 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
