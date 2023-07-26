a2d15d135f0b6945cdeb6dc426c59e6ac6e67705
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 05:52:49 -0700

    commit by file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,23 +5,29 @@
 void times_table(void)
 {
 	int i = 0;
-	int j = 0;
+	int j = 1;
+	int k;
 
 	while (i <= 9)
 	{
+		_putchar('0');
 		while(j <= 9)
 		{
-			_putchar(48 + (i * j));
-			if(j != 9)
+			k = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if((k / 10) == 0)
 			{
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(',');
+			} else
+			{
+				_putchar(48 + (k / 10));
 			}
+			_putchar(48 + (k % 10));
 			j++;
 		}
 		i++;
-		j = 0;
+		j = 1;
 		_putchar('\n');
 	}
 }
