632921bd7ff49862b6723d13999065f53e6f7c05
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-26 17:08:06 -0700

    commit by file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz problem
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if ((x % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((x % 3) == 0)
+			printf("Fizz ");
+		else if ((x % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", x);
+	}
+	printf("Buzz\n");
+}
