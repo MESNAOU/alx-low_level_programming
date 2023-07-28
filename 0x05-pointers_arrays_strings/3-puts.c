e7cb9a9119b687bdec1a548ef2634e94314d61dc
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 08:20:45 -0700

    commit by file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,19 @@
 #include "main.h"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * _strlen - prints a string
+ * @s: string's pointer
  */
 
+void _puts(char *str)
+{
+	int i = 0;
+	char c = *str;
 
+	while (c != '\0')
+	{
+		_putchar(c);
+		c = *(s + i);
+	}
+	_putchar('\n');
+}
