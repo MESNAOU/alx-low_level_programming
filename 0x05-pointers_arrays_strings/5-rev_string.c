a544733930a42b143ece4afb4e4fd36a597429b5
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 09:19:36 -0700

    commit by file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,23 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * rev_string - prints a string
+ * @s: string's pointer
  */
 
+void rev_string(char *s)
+{
+	int end = _strlen(s) - 1;
+	int start = 0;
+	char c;
 
+	while (start < end)
+	{
+		c = *(s + end);
+		*(s + end) = *(s + start);
+		*(s + start) = c;
+		end--;
+		start++;
+	}
+}
