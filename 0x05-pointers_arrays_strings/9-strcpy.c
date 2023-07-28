83b814917639aefe0695538063166a0bf9fd27f3
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 10:59:25 -0700

    commit by file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * fuction - desc
- * @vars
+ * _strcpy - copy a string
+ * @dest: pointing the destination 
+ * @src: pointing the source
  *
- * Return:
+ * Return: destination's pointer
  */
 
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	char c = *(src + i);
 
+	while (c != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return &dest;
+}
