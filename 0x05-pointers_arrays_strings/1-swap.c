e96512f9398bc9075087681337fb7b021a59b5a8
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 07:57:54 -0700

    commit by file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * swap_int - swap 2 integers
+ * @a: integer1
+ *@b: integer2
  */
+void swap_int(int *a, int *b)
+{
+	int c;
 
+	c = *a;
+	*a = *b;
+	*b = c;
+}
 
