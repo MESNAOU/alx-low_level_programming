6f3fdef782ffcb695f0418ecc2a024bf9956cbec
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 09:26:29 -0700

    commit by file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,18 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * puts2 - prints a string in even
+ * @str: string's pointer
  */
 
+void puts2(char *str)
+{
+	int i;
 
+	for (i =0; i < _strlen(str); i += 2)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
