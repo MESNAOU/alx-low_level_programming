7e64950f4634cdebd783ac56fae7a8721a141458
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 11:16:46 -0700

    commit by file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcpy - copy a string
- * @dest: pointing the destination 
+ * @dest: pointing the destination
  * @src: pointing the source
  *
  * Return: destination's pointer
@@ -20,6 +20,6 @@ char *_strcpy(char *dest, char *src)
 		c = *(src + i);
 	}
 	*(dest + i) = '\0';
-	return dest;
+	return (dest);
 }
 
