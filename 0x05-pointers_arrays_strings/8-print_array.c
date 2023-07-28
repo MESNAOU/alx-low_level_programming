de2b0e2b7e3ca18ed3e1b3d416839e3f94b0bd64
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 10:42:22 -0700

    commit by file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d",*(a + i));
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", *(a + i));
 	}
-	printf("%d",*(a + n - 1));
-	_putchar('\n');
+	printf("%d\n", *(a + n - 1));
 }
-
