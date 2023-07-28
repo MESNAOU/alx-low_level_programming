62e6ff22926ff4e9ade29cb6413d48ee9681d3f2
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 10:08:45 -0700

    commit by file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * print_array - prints an array's elements
+ * @a: array
+ * @n: number of elements to print
  */
 
+void print_array(int *a, int n)
+{
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		_putchar(*(a + i));
+	}
+	_putchar('\n');
+}
