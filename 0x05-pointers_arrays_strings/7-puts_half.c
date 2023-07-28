3566185e0ee19445d1f4fc5e8acc3b14f5c6c302
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 09:52:07 -0700

    commit by file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,30 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * puts2 - prints a string in even
+ * @str: string's pointer
  */
 
+void puts2(char *str)
+{
+	int nb = _strlen(str) / 2;
+	int i;
+	int c;
+	int r = _strlen(str) % 2;
 
+	switch (r)
+	{
+		case 0:
+			c = nb;
+			break;
+		case 1:
+			c = nb + 1;
+			break;
+	}
+	for (i = 0; i < nb; i++)
+	{
+		_putchar(*(str + c + i));
+	}
+	_putchar('\n');
+}
