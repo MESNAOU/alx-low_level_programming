575f5b2077771d78add182f4296ac0c5fdc61e93
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-28 09:04:40 -0700

    commit by file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /**
- * fuction - desc
- * @vars
- *
- * Return:
+ * print_rev - prints a string
+ * @s: string's pointer
  */
 
+void print_rev(char *s)
+{
+	int i = _strlen(char *s) - 1;
 
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
