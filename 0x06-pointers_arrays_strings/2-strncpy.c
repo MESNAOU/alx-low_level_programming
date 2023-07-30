32432df40afc001d90bedd2c7c5bacdd1ee3883e
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-30 05:14:40 -0700

    commit by file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,25 @@
 #include "main.h"
-
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * _strncpy - copy n strings
+ * @dest: destination's pointer
+ * @src: source's pointer
+ * @n: number of char to copy
+ *
+ * Return: destination's pointer
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char c = *(src + i);
+
+	while ((c != '\0') && (i < n))
+	{
+		*(dest + i) = c;
+		i++;
+		c = *(src + i);
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
 
