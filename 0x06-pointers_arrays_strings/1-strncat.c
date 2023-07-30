fcb01c0f8dfdb8b06d8dd4739c695fea800dd87c
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-30 04:59:36 -0700

    commit by file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,32 @@
 #include "main.h"
-
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * _strncat - cancate 2 strings
+ * @dest: destination's pointer
+ * @src: source's pointer
+ * @n: number of char to cancate
+ *
+ * Return: destination's pointer
  */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int k = 0;
+	int i = 0;
+	char c = *(dest + k);
+
+	while (c != '\0')
+	{
+		k++;
+		c = *(dest + k);
+	}
+	c = *(src + i);
+	while ((c != '\0') && (i < n))
+	{
+		*(dest + i + k) = *(src + i);
+		i++;
+		c = *(src + i);
+	}
+	*(dest + i + k) = '\0';
+	return (dest);
+}
 
