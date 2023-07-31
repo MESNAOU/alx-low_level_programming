c92d3492e47c6ea24b95ae7c72e72f1b0ed6dc90
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-31 01:49:53 +0100

    commit by file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,24 @@
 #include "main.h"
 
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * _strcmp - coùpare 2 strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: camparaision
  */
 
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int r;
 
+	do
+	{
+		r = *(s1 + i) - *(s2 + i);
+	} while (r == 0)
+	{
+		i++;
+	}
+	return (r);
+}
