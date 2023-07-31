dfc6b55958b17f91f900999493406e15e302d3ea
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-31 18:18:08 +0100

    commit by file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,29 @@
 #include "main.h"
 
+int separ(char c, char *a);
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * *leet - chaging some characters to numbers.
+ * @s: string
+ *
+ * Return: string
  */
+char *leet(char *s)
+{
+	char exc[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i = 0;
+	char c = *s;
+	int k;
 
+	while (c != '\0')
+	{
+		for (k = 0; k < 10; k +=2)
+		{
+			if (*(exc + k) == c)
+				*(s + i) = *(exc + k + 1);
+		}
+		i++;
+		c = *(s + i);
+	}
+	return (s);
+}
 
