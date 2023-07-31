69920b661367966c5ed9cffbccad396fcade7686
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-31 05:20:16 -0700

    commit by file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,23 @@
 #include "main.h"
 
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: array's size
  */
 
+void reverse_array(int *a, int n)
+{
+	int end = n - 1;
+	int start = 0;
+	int c;
 
+	while (start < end)
+	{
+		c = *(a + end);
+		*(a + end) = *(a + start);
+		*(a + start) = c;
+		end--;
+		start++;
+	}
+}
