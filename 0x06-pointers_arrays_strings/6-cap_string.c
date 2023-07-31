80dc58178753689104b4512f874561fcdd0a2348
MESNAOU <mesnaoui2002@outlook.fr>
2023-07-31 06:57:57 -0700

    commit by file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,59 @@
 #include "main.h"
 
+int separ(char c, char *a);
 /**
- * reset_to_98 - change the pointed value
- * @n: pointed value
+ * cap_string - capitalizes all words of a string.
+ * @s: string
+ *
+ * Return: string
  */
 
+char *cap_string(char *s)
+{
+	char sep[13] = {' ',
+		'\t',
+		'\n',
+		',',
+		';',
+		'.',
+		'!',
+		'?',
+		'"',
+		'(',
+		')',
+		'{',
+		'}'};
+	int i = 0;
+	char c = *s;
 
+	while (c != '\0')
+	{
+		if (separ(c, sep))
+		{
+			if (*(s + i + 1) <= 'z' && *(s + i + 1) >= 'a')
+				*(s + i + 1) = *(s + i + 1) + 32;
+		}
+		i++;
+		c = *(s + i);
+	}
+	return (s);
+}
+
+/**
+ * separ - check if it's a separator
+ * @c: character  to check
+ * @a: separetor's array
+ *
+ * Return: bool
+ */
+int separ(char c, char *a)
+{
+	int i = 0;
+
+	for (i = 0; i < 13; i++)
+	{
+		if (*(a + i) == c)
+			return (1);
+	}
+	return (0);
+}
