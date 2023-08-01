834b1f26db2f50e5a1269283caaf5c92106d5b14
MESNAOU <mesnaoui2002@outlook.fr>
2023-08-01 14:01:24 -0700

    commit by file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_chessboard - modelize a chessboard
  * @a: board
@@ -11,8 +11,8 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar(' ');
+			printf("%c", a[i][j]);
+		printf("\n");
 	}
 }
 
