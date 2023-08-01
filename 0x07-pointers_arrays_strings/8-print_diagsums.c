ad733819fd8b7dca3bbc4d633ee743f9c203fa2b
MESNAOU <mesnaoui2002@outlook.fr>
2023-08-01 16:40:54 -0700

    commit by file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,15 +10,11 @@ void print_diagsums(int *a, int size)
 	int i;
 	int s1 = 0;
 	int s2 = 0;
-	int *p1, *p2;
 
 	for (i = 0; i < size; i++)
 	{
-		p1 = a + (i * size + i) * 4;
-		p2 = a + (i * size + size - i - 1) * 4;
-		s1 += *p1;
-		s2 += *p2;
+		s1 += *(a + (i * size + i));
+		s2 += *(a + (i * size + size - i - 1));
 	}
 	printf("%d, %d\n", s1, s2);
 }
-
