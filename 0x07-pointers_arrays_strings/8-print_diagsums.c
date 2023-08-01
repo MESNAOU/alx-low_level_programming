6d8bae9c4471288f117c9d3c7128ed6440972dfe
MESNAOU <mesnaoui2002@outlook.fr>
2023-08-01 15:15:19 -0700

    commit by file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,16 +5,19 @@
  * @a: square matrix
  * @size: size
  */
-void print_diagsums(int *a[], int size)
+void print_diagsums(int *a, int size)
 {
 	int i;
 	int s1 = 0;
 	int s2 = 0;
+	int *p1, *p2;
 
 	for (i = 0; i < size; i++)
 	{
-		s1 += a[i][i];
-		s2 += a[i][size - i - 1];
+		p1 = a + (i * size + i) * 4;
+		p2 = a + (i * size + size - i - 1) * 4;
+		s1 += *p1;
+		s2 += *p2;
 	}
 	printf("%d, %d", s1, s2);
 }
