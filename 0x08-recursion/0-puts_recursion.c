6f1f771cd5d644f8ee75f72148f68c889456ab6f
MESNAOU <mesnaoui2002@outlook.fr>
2023-08-03 09:54:09 -0700

    commit by file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  fills the n bytes of memory area pointed to by s with b
+ * @s: string to fill
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	_putchar(*s);
+	_put_recursion(*(s + 1));
+}
+
