11e891dbd9a5ed4b4142d484031f792763b6becb
MESNAOU <mesnaoui2002@outlook.fr>
2023-08-06 07:35:35 -0700

    commit by file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+int devide(int n, int x);
+/**
+ * is_prime_number - check if it's a prime number
+ * @n: number
+ *
+ * Return: boolean
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	} else if (n == 2)
+		return (1);
+	return (divise(n, 2));
+}
+/**
+ * devide - test if x devide n
+ * @n: number
+ * @x: supposed divisible
+ *
+ * Return: bool
+ */
+int divide(int n, int x)
+{
+	int p = n % x;
+
+	if (p == 0)
+	{
+		return (0);
+	} else if ((x + 1) < n / 2)
+		return (divide(n, x + 1));
+	return (1);
+}
